0a433f67d841b3e3034dcf3c35df3d5a2d6504bb
Brenton Gunning <mail@brentongunning.com>
2024-11-03 13:41:48 -0700

    InterpreterImpl skeleton

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,8 +1,51 @@
-use crate::{errors::ExecuteError, sig_verifier::SigVerifier, vm::Vm};
+use crate::{core::Tx, errors::ExecuteError, sig_verifier::SigVerifier, vm::Vm};
 
 pub trait Interpreter {
     type SigVerifier: SigVerifier;
     type Vm: Vm;
 
-    fn execute(script: &[u8]) -> Result<(), ExecuteError>;
+    fn execute(&mut self, tx: &Tx) -> Result<(), ExecuteError>;
+}
+
+pub struct InterpreterImpl<S: SigVerifier, V: Vm> {
+    _sig_verifier: S,
+    _vm: V,
+}
+
+impl<S: SigVerifier, V: Vm> InterpreterImpl<S, V> {
+    pub fn new(sig_verifier: S, vm: V) -> Self {
+        Self {
+            _sig_verifier: sig_verifier,
+            _vm: vm,
+        }
+    }
+}
+
+impl<S: SigVerifier, V: Vm> Interpreter for InterpreterImpl<S, V> {
+    type SigVerifier = S;
+    type Vm = V;
+
+    fn execute(&mut self, _tx: &Tx) -> Result<(), ExecuteError> {
+        unimplemented!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{core::Tx, sig_verifier::SigVerifierImpl, stack::StackImpl, vm::VmImpl};
+
+    #[test]
+    #[should_panic]
+    fn test_interpreter() {
+        let tx = Tx {
+            version: 1,
+            script: vec![0x01, 0x02, 0x03],
+        };
+        let sig_verifier = SigVerifierImpl::new(&tx);
+        let stack = StackImpl::default();
+        let vm = VmImpl::new(stack);
+        let mut interpreter = InterpreterImpl::new(sig_verifier, vm);
+        interpreter.execute(&tx).ok();
+    }
 }
